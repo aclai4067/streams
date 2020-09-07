a18e2365fc74a462ca222bee9a818a2b9ae4e70c
Ashley Claiborne <languageisaliquid@gmail.com>
2020-09-06 23:00:34 -0500

    parsing file lines to class

---
+++ streams/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace streams
+{
+    public class Account
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AccountNumber { get; set; }
+        public string PrimaryAddress { get; set; }
+        public string SecondaryAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public decimal Charges { get; set; }
+        public decimal Payments { get; set; }
+        public decimal Balance 
+        {
+            get
+            {
+                return Charges - Payments;
+            }
+        }
+
+
+    }
+}
--- streams/Program.cs
+++ streams/Program.cs
@@ -14,8 +14,8 @@ namespace streams
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "people.txt");
-            var fileContents = ReadFile(fileName);
-            Console.WriteLine(fileContents);
+            var fileContents = ReadAcctLines(fileName);
+
 
             // --- get file list ---
             //var files = directory.GetFiles("*.txt");
@@ -34,6 +34,14 @@ namespace streams
             //        Console.WriteLine(Console.ReadLine());
             //    }
             //}
+
+            // --- read all file lines ---
+            //var fileContents = ReadFile(fileName);
+            //string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            //foreach (var line in fileLines)
+            //{
+            //    Console.WriteLine(line);
+            //}
         }
 
         public static string ReadFile(string fileName)
@@ -42,6 +50,43 @@ namespace streams
             {
                 return reader.ReadToEnd();
             }
+        }
+
+        public static List<Account> ReadAcctLines(string fileName)
+        {
+            var accounts = new List<Account>();
+
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                reader.ReadLine();
+                while((line = reader.ReadLine()) != null)
+                {
+                    var acct = new Account();
+                    string[] values = line.Split(',');
+                    acct.FirstName = values[0];
+                    acct.LastName = values[1];
+                    acct.AccountNumber = values[2];
+                    acct.PrimaryAddress = values[3];
+                    acct.SecondaryAddress = values[4];
+                    acct.City = values[5];
+                    acct.State = values[6];
+                    acct.Zip = values[7];
+                    decimal amount;
+                    if (decimal.TryParse(values[8], out amount))
+                    {
+                        acct.Charges = amount;
+                    }
+                    if (decimal.TryParse(values[9], out amount))
+                    {
+                        acct.Payments = amount;
+                    }
+
+                    accounts.Add(acct);
+                }
+            }
+
+            return accounts;
         }
     }
 }
